a2f7f599a47c3fa43b51dca1c0c6fd4d0b5111f2
pzalan <palan7367@gmail.com>
2026-05-18 21:30:03 -0700

    Added the character selection, Main Menu and updated the player movement script with anyting i needed for the characters to update

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -15,11 +16,39 @@ public class PlayerMovement : MonoBehaviour
     private float dashingTime = 0.2f;
     private float dashingCoolDown = 1f; 
 
+    public CharacterData characterD; //this will be for the character and the next 2 variables
+    public SpriteRenderer artworkSprite;
+    private int selectedOption = 0;
+
 
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("selectedOption")) //this will check if there is a saved data or will give the player the character at 0
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
+        UpdateCharacter(selectedOption);
+    }
+
+    private void UpdateCharacter(int selectedOption) // gets the name and character from the character data and updates it
+    {
+        Character character = characterD.GetCharacter(selectedOption);
+        artworkSprite.sprite = character.characterSprite;
+    }
+
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption"); //sets the value of selectedOption to whatever number the key is
+    }
 
     void Update()
     {
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+
+[System.Serializable] //will be for our character datatype
+public class Character
+{
+        public string characterName;
+        public Sprite characterSprite;
+}
