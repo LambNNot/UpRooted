ceec9d7482c9fd20e098ed7b19820e4405c958d9
pzalan <palan7367@gmail.com>
2026-05-26 11:16:19 -0700

    jusrrfg

--- Assets/Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu.cs
@@ -10,10 +10,16 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public string NewGameScene; // this will be to input scene the player will go once button is pressed
+    public string HelpOrLoreScene; // this will be to input the scene that will take the player to the lore or help scene
 
     public void NewGame()
     {
         SceneManager.LoadScene(NewGameScene);
+    }
+
+    public void HelpOrLore()
+    {
+        SceneManager.LoadScene(HelpOrLoreScene);
     }
 
     public void QuitGame()
