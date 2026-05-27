34d98178a87df7c10b4bf206f443843a72b89386
AdrianS2k <adriansancheza7@gmail.com>
2026-05-27 11:37:03 -0700

    added NPC interaction at the end of level 1, sends npc to level selector

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    private Vector3 offset = new Vector3(0f, 1.75f, -10f);
+    private Vector3 offset = new Vector3(0f, 1.6f, -10f);
     private float smoothTime = 0.25f; // time to reach target
     private Vector3 velocity = Vector3.zero; 
     [SerializeField] private Transform target; // the player that the camera will follow 
