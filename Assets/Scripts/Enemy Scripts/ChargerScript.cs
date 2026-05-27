da1849b8be31904b8e7d89eaa4c69612a94568ec
LambNNot <nsphan1207@gmail.com>
2026-05-27 14:17:35 -0700

    feat: charger done

--- Assets/Scripts/Enemy Scripts/ChargerScript.cs
+++ Assets/Scripts/Enemy Scripts/ChargerScript.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChargerScript : EnemyBase
 {
     [SerializeField]
     private float chargeDelay = 0.5f;
+
     [SerializeField]
     private float chargeDuration = 1.5f;
+
     [SerializeField]
     private float chargeSpeed = 15f;
+
     [SerializeField]
     private float sightDistance = 5f;
 
@@ -31,97 +33,162 @@ public class ChargerScript : EnemyBase
     [SerializeField]
     private List<GameObject> targets = new List<GameObject>();
 
+    [SerializeField]
+    private Transform raycastOrigin;
+
     private bool isCharging = false;
     private bool isWaiting = false;
     private float chargeTimer = 0f;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         sr.color = walkColor;
+
+        Debug.Log("=== Charger Started ===");
+        Debug.Log("Target Layer Mask Value: " + targetLayers.value);
+        Debug.Log("Sight Distance: " + sightDistance);
+        Debug.Log("Charge Speed: " + chargeSpeed);
     }
 
     // Update is called once per frame
     protected override void Update()
     {
-        if (isWaiting) {
+
+        if (isWaiting)
+        {
             return;
         }
+
         if (isCharging)
         {
             ChargeForward();
+
             chargeTimer += Time.deltaTime;
+
+
             if (chargeTimer > chargeDuration)
             {
                 stopCharging();
             }
+
             return;
         }
 
         Walk();
+
+        UpdateSpriteDirection();
+
         LookAhead();
     }
 
     private void stopCharging()
     {
+
         sr.color = walkColor;
         isCharging = false;
         chargeTimer = 0f;
+
         TurnAround();
     }
 
     private void beginCharging()
     {
+
         isCharging = true;
         isWaiting = false;
+
         sr.color = chargeColor;
     }
 
     private IEnumerator WaitCharge()
     {
+
         yield return new WaitForSeconds(chargeDelay);
+
+
         beginCharging();
     }
 
     private void PrepareCharge()
-    { 
+    {
+
+        if (isWaiting || isCharging)
+        {
+            return;
+        }
+
         sr.color = prepareColor;
+
         isWaiting = true;
+
+
         Invoke(nameof(beginCharging), chargeDelay);
-        
     }
 
     private void ChargeForward()
     {
-        transform.position += getMoveDirection() * chargeSpeed * Time.deltaTime;
+        Vector2 dir = ((Vector2)getMoveDirection()).normalized;
+
+        Vector3 movement = dir * chargeSpeed * Time.deltaTime;
+
+        transform.position += movement;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (isCharging)
         {
+
             stopCharging();
+
             return;
         }
-        TurnAround();
 
+
+        TurnAround();
     }
 
     private void LookAhead()
     {
-        RaycastHit2D hit = Physics2D.Raycast(
+        Vector2 dir = ((Vector2)getMoveDirection()).normalized;
+
+        Debug.DrawRay(
             transform.position,
-            getMoveDirection(),
+            dir * sightDistance,
+            Color.red
+        );
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            raycastOrigin.position,
+            dir,
             sightDistance,
             targetLayers
         );
 
         if (hit.collider != null)
         {
-            Debug.Log("Saw: " + hit.collider.gameObject.name);
+
             PrepareCharge();
         }
+    }
+
+    private void UpdateSpriteDirection()
+    {
+        Vector3 scale = transform.localScale;
+
+        // Looking right
+        if (getMoveDirection().x > 0)
+        {
+            scale.x = -Mathf.Abs(scale.x);
+        }
+        // Looking left
+        else if (getMoveDirection().x < 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
     }
 }
