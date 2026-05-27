b76b1c6cf284e1300e905783028bfcb774cfae01
AdrianS2k <adriansancheza7@gmail.com>
2026-05-26 21:07:00 -0700

    Added the fast-fall implementation to the character controller, also added another script for the player to be able to properly interact with oneway platforms. Finally updated the camera offset to show less area under player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -15,11 +16,39 @@ public class PlayerMovement : MonoBehaviour
     private float dashingTime = 0.2f;
     private float dashingCoolDown = 1f; 
 
+    public SpriteRenderer artworkSprite;
+    private int selectedOption = 0;
+    private float fastFallMultiplier = 10.0f; // fast fall mechanics 
+    private float maxFallSpeed = -40f;
 
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("selectedOption")) //this will check if there is a saved data or will give the player the character at 0
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
+        // UpdateCharacter(selectedOption);
+    }
+
+    // private void UpdateCharacter(int selectedOption) // gets the name and character from the character data and updates it
+    // {
+    //     Character character = characterD.GetCharacter(selectedOption);
+    //     artworkSprite.sprite = character.characterSprite;
+    // }
+
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption"); //sets the value of selectedOption to whatever number the key is
+    }
 
     void Update()
     {
@@ -44,13 +73,23 @@ public class PlayerMovement : MonoBehaviour
         Flip();
     }
 
-    private void FixedUpdate()
-    
+    private void FixedUpdate(){
+    if (isDashing)
+        return;
+
+    rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
+
+    // Fast fall when pressing down while in the air
+    if (!IsGrounded() && Input.GetAxisRaw("Vertical") < 0f && rb.linearVelocity.y < 0f)
     {
-        if(isDashing)
-            return;
-        rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
+        float newYVelocity = rb.linearVelocity.y + Physics2D.gravity.y * (fastFallMultiplier - 1f) * Time.fixedDeltaTime;
+
+        // Prevent falling infinitely fast
+        newYVelocity = Mathf.Max(newYVelocity, maxFallSpeed);
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, newYVelocity);
     }
+}
 
     private bool IsGrounded()
     {
