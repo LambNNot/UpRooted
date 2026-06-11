74a6ec0ae24a485fd5437d36c9da04016672ed7f
pzalan <115607499+pzalan@users.noreply.github.com>
2026-06-10 23:49:25 -0700

    Each level now has one gate instead of two, and now the gate will only be on, once the player kills all the enemies/ fills the progressbar

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -7,24 +7,42 @@ public class Gate : MonoBehaviour
     public string sceneToLoad;
 
     [Header("Gate State")]
-    public bool unlocked = true;
+    public bool unlocked = false;
 
     private bool playerInside = false;
+    
+    public bool passprogressbar = false; //this will be so then the gate could be activated
+    private SpriteRenderer spriteRenderer; 
+    public ProgressBar levelProgressBar; 
 
     void Start()
     {
-        if(unlocked == true)
-        {
-            GetComponent<SpriteRenderer>().color = Color.softYellow;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(passprogressbar){ // this will be for the gates in the level selector
+            unlocked = true;
         }
-        else
-        {
-            GetComponent<SpriteRenderer>().color = Color.grey;
+        else{
+            unlocked = false; // will be locked if its a normal level gate
+
+            if (levelProgressBar == null && Level2ProgressBar.Instance == null) // will look for a progress bar 
+            {
+                levelProgressBar = FindFirstObjectByType<ProgressBar>();
+            }
         }
+
+        UpdateGateColor();
     }
 
     void Update()
     {
+        if(!passprogressbar && !unlocked && CheckIfProgressIsFull()){ //this will activate the gate when the progressBar is full and is not a gate in the level selector
+            unlocked = true; 
+            UpdateGateColor();
+            Debug.Log("ProgressBar is full, now the gate unlocked.");
+
+        }
+
         if (playerInside && unlocked)
         {
             if (Input.GetKeyDown(KeyCode.E)){
@@ -51,5 +69,28 @@ public class Gate : MonoBehaviour
     private void LoadNextLevel()
     {
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    private bool CheckIfProgressIsFull(){
+        if(Level2ProgressBar.Instance != null){ // this will check the progress bar in the levels and the level2progressbar
+            return Level2ProgressBar.Instance.slider.value >= Level2ProgressBar.Instance.TotalEnemies;
+        }
+        if(levelProgressBar != null){
+            return levelProgressBar.slider.value >= levelProgressBar.TotalEnemies;
+        }
+
+        return false;
+    }
+
+    private void UpdateGateColor(){
+        if(spriteRenderer != null){
+
+            if(unlocked){ //this will change the color depending on the state
+                spriteRenderer.color = Color.softYellow;
+            }
+            else{
+                spriteRenderer.color = Color.grey;
+            }
+        }
     }
 }
