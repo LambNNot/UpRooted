0a5237bf9d49606ba697283e53d9bfc46e746a96
LambNNot <nsphan1207@gmail.com>
2026-06-12 09:31:32 -0700

    walking animation done

--- Assets/Scripts/Squidcap Scripts/SquidcapMovement.cs
+++ Assets/Scripts/Squidcap Scripts/SquidcapMovement.cs
@@ -2,9 +2,28 @@ using UnityEngine;
 
 public class SquidcapMovement : PlayerMovementBase
 {
+
+    [SerializeField] Animator animator;
+
     protected override void Start()
     {
         base.Start();
         isFacingRight = false;
+        if (animator == null)
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+    }
+
+     protected override void Update()
+    {
+        base.Update();
+
+        bool isMoving =
+            Mathf.Abs(horizontalInput) > 0.01f &&
+            !isDashing &&
+            (combat == null || !combat.isRecoiling);
+
+        animator.SetBool("isMoving", isMoving);
     }
 }
